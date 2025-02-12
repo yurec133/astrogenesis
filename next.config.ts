23b988ac0ebe168778c0ff29c74188fec22fbee2
Yurec <yurec133@gmail.com~>
2025-02-12 20:06:41 +0200

    fix (config)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
+  output: "export",
   basePath: isProd ? "/astrogenesis" : "",
   assetPrefix: isProd ? "/astrogenesis/" : "",
   trailingSlash: true,
-  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
