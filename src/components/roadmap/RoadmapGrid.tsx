f4b8cfcb01159cec1eab8c8a0e4046c9751389cf
Yurec <yurec133@gmail.com~>
2025-02-20 11:01:18 +0200

    fix (rood map)

--- src/components/roadmap/RoadmapGrid.tsx
+++ src/components/roadmap/RoadmapGrid.tsx
@@ -33,14 +33,14 @@ const roadmapData = [
 const RoadmapGrid = () => {
   return (
     <div className="relative">
-      <div className="absolute z-10 left-5 right-0 top-[72px] h-[10vw] max-w-full w-[calc(100%-70px)] bg-[url('/images/bg-timeline-bar.svg')] bg-no-repeat bg-contain hidden lg:block">
-        <div className="absolute left-[-58px] top-[-130px] w-[69px] h-[113px] bg-[url('/images/img-rocket-in.svg')] bg-no-repeat"></div>
-        <div className="absolute right-[-110px] top-[24%] w-[106px] h-[64px] bg-[url('/images/img-rocket-out.svg')] bg-no-repeat"></div>
+      <div className="absolute z-10 left-5 right-0 top-[72px] h-[10vw] max-w-full w-[calc(100%-70px)] bg-[url('/astrogenesis/images/bg-timeline-bar.svg')] bg-no-repeat bg-contain hidden lg:block">
+        <div className="absolute left-[-58px] top-[-130px] w-[69px] h-[113px] bg-[url('/astrogenesis/images/img-rocket-in.svg')] bg-no-repeat"></div>
+        <div className="absolute right-[-110px] top-[24%] w-[106px] h-[64px] bg-[url('/astrogenesis/images/img-rocket-out.svg')] bg-no-repeat"></div>
       </div>
 
-      <div className="absolute z-10 right-[40px] top-[-20px] h-[812px] w-[103px] bg-[url('/images/bg-timeline-bar-vertical.svg')] bg-no-repeat lg:hidden">
-        <div className="absolute left-[-105px] top-[-68px] w-[69px] h-[113px] bg-[url('/images/img-rocket-in.svg')] bg-no-repeat rotate-[83deg] scale-y-[-1]"></div>
-        <div className="absolute bottom-[-60px] left-[-20px] w-[106px] h-[64px] bg-[url('/images/img-rocket-out.svg')] bg-no-repeat rotate-[170deg]"></div>
+      <div className="absolute z-10 right-[40px] top-[-20px] h-[812px] w-[103px] bg-[url('/astrogenesis/images/bg-timeline-bar-vertical.svg')] bg-no-repeat lg:hidden">
+        <div className="absolute left-[-105px] top-[-68px] w-[69px] h-[113px] bg-[url('/astrogenesis/images/img-rocket-in.svg')] bg-no-repeat rotate-[83deg] scale-y-[-1]"></div>
+        <div className="absolute bottom-[-60px] left-[-20px] w-[106px] h-[64px] bg-[url('/astrogenesis/images/img-rocket-out.svg')] bg-no-repeat rotate-[170deg]"></div>
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-6 gap-4">
